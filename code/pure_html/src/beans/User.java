e4c5ba9c4c0f730464c5a9ada482f4e5a1a57876
Francesco Paterna <francesco.paterna@mail.polimi.it>
2021-04-17 16:08:22 +0200

    update v1.01

---
+++ code/pure_html/src/beans/User.java
@@ -0,0 +1,48 @@
+package beans;
+
+public class User {
+	private int id;
+	private String role;
+	private String name;
+	private String surname;
+	private String email;
+	private String coursedeg;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCoursedeg() {
+		return coursedeg;
+	}
+	public void setCoursedeg(String coursedeg) {
+		this.coursedeg = coursedeg;
+	}
+
+}
---
+++ code/pure_html/src/dao/UserDAO.java
@@ -0,0 +1,40 @@
+package dao;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import beans.User;
+
+public class UserDAO {
+	private Connection con;
+
+	public UserDAO(Connection connection) {
+		this.con = connection;
+	}
+
+	public User checkCredentials(String id, String pwd) throws SQLException {
+		String query = "SELECT  id, username, name, surname FROM student  WHERE username = ? AND password =?";
+		try (PreparedStatement pstatement = con.prepareStatement(query);) {
+			pstatement.setString(1, id);
+			pstatement.setString(2, pwd);
+			try (ResultSet result = pstatement.executeQuery();) {
+				if (!result.isBeforeFirst()) // no results, credential check failed
+					return null;
+				else {
+					result.next();
+					User user = new User();
+					user.setId(result.getInt("id"));
+					user.setName(result.getString("name"));
+					user.setSurname(result.getString("surname"));
+					user.setEmail(result.getString("email"));
+					if(result.getString("role").equals("student") ) {
+						user.setCoursedeg(result.getString("coursedeg"));
+					}
+					return user;
+				}
+			}
+		}
+	}
+}
