2637bcb070decaa123279054d003bf8dbd8b0984
andrea_sanchini <andrea.sanchini@mail.polimi.it>
2021-04-27 11:54:34 +0200

    fix: fixed some methods in EnrollsDAO
    new: added new methods to find all enrollments ordered by mark

--- code/pure_html/src/dao/EnrollsDAO.java
+++ code/pure_html/src/dao/EnrollsDAO.java
@@ -17,12 +17,36 @@ public class EnrollsDAO {
 		this.connection = connection;
 	}
 
+	public List<Enroll> FindEnrolls(int exameDateId) throws SQLException {
+		List<Enroll> enrolls = new ArrayList<>();
+
+		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDexam = ?";
+		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
+			pstatement.setInt(1, exameDateId);
+			try (ResultSet result = pstatement.executeQuery();) {
+				while (result.next()) {
+					Enroll enroll = new Enroll();
+					enroll.setIDstudent(result.getInt("ID"));
+					enroll.setName(result.getString("name"));
+					enroll.setName(result.getString("surname"));
+					enroll.setMail(result.getString("email"));
+					enroll.setMark(result.getString("mark"));
+					enroll.setStatus(Status.valueOf(result.getString("status")));
+					enrolls.add(enroll);
+				}
+			}
+		}
+		return enrolls;
+	}
+
 	public List<Enroll> FindEnrollsOrderedByIDAsc(int exameDateId) throws SQLException {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY user.ID ASC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDexam = ? ORDER BY user.ID ASC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -45,8 +69,8 @@ public class EnrollsDAO {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY user.ID DESC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDexam = ? ORDER BY user.ID DESC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -69,8 +93,8 @@ public class EnrollsDAO {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY user.name ASC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY user.name ASC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -93,8 +117,8 @@ public class EnrollsDAO {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY user.name DESC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY user.name DESC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -117,8 +141,8 @@ public class EnrollsDAO {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY user.surname ASC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY user.surname ASC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -141,8 +165,8 @@ public class EnrollsDAO {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY user.surname DESC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY user.surname DESC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -165,8 +189,8 @@ public class EnrollsDAO {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY user.email ASC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY user.email ASC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -189,8 +213,8 @@ public class EnrollsDAO {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY user.email DESC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY user.email DESC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -209,52 +233,64 @@ public class EnrollsDAO {
 		return enrolls;
 	}
 
-	/*
-	 * public List<Enroll> FindEnrollsOrderedByMarkAsc(int exameDateId) throws
-	 * SQLException { List<Enroll> enrolls = new ArrayList<>();
-	 * 
-	 * String query =
-	 * "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-	 * +
-	 * "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-	 * + "WHERE examedate.ID = ? ORDER BY user.email ASC"; try (PreparedStatement
-	 * pstatement = connection.prepareStatement(query);) { pstatement.setInt(1,
-	 * exameDateId); try (ResultSet result = pstatement.executeQuery();) {
-	 * while(result.next()) { Enroll enroll = new Enroll();
-	 * enroll.setIDstudent(result.getInt("ID"));
-	 * enroll.setName(result.getString("name"));
-	 * enroll.setName(result.getString("surname"));
-	 * enroll.setMail(result.getString("email"));
-	 * enroll.setMark(result.getString("mark"));
-	 * enroll.setStatus(Status.valueOf(result.getString("status")));
-	 * enrolls.add(enroll); } } } return enrolls; }
-	 */
-	/*
-	 * public List<Enroll> FindEnrollsOrderedByMarkDesc(int exameDateId) throws
-	 * SQLException { List<Enroll> enrolls = new ArrayList<>();
-	 * 
-	 * String query =
-	 * "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-	 * +
-	 * "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-	 * + "WHERE examedate.ID = ? ORDER BY user.email ASC"; try (PreparedStatement
-	 * pstatement = connection.prepareStatement(query);) { pstatement.setInt(1,
-	 * exameDateId); try (ResultSet result = pstatement.executeQuery();) {
-	 * while(result.next()) { Enroll enroll = new Enroll();
-	 * enroll.setIDstudent(result.getInt("ID"));
-	 * enroll.setName(result.getString("name"));
-	 * enroll.setName(result.getString("surname"));
-	 * enroll.setMail(result.getString("email"));
-	 * enroll.setMark(result.getString("mark"));
-	 * enroll.setStatus(Status.valueOf(result.getString("status")));
-	 * enrolls.add(enroll); } } } return enrolls; }
-	 */
+	public List<Enroll> FindEnrollsOrderedByMarkAsc(int exameDateId) throws SQLException {
+		List<Enroll> enrolls = new ArrayList<>();
+
+		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) "
+				+ "JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY FIELD(mark, 'ABSENT', 'RIMANDATO', 'RIPROVATO', '18', "
+				+ "'19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '30L') ASC";
+		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
+			pstatement.setInt(1, exameDateId);
+			try (ResultSet result = pstatement.executeQuery();) {
+				while (result.next()) {
+					Enroll enroll = new Enroll();
+					enroll.setIDstudent(result.getInt("ID"));
+					enroll.setName(result.getString("name"));
+					enroll.setName(result.getString("surname"));
+					enroll.setMail(result.getString("email"));
+					enroll.setMark(result.getString("mark"));
+					enroll.setStatus(Status.valueOf(result.getString("status")));
+					enrolls.add(enroll);
+				}
+			}
+		}
+		return enrolls;
+	}
+
+	public List<Enroll> FindEnrollsOrderedByMarkDesc(int exameDateId) throws SQLException {
+		List<Enroll> enrolls = new ArrayList<>();
+
+		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) "
+				+ "JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY FIELD(mark, 'ABSENT', 'RIMANDATO', 'RIPROVATO', '18', "
+				+ "'19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '30L') DESC";
+		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
+			pstatement.setInt(1, exameDateId);
+			try (ResultSet result = pstatement.executeQuery();) {
+				while (result.next()) {
+					Enroll enroll = new Enroll();
+					enroll.setIDstudent(result.getInt("ID"));
+					enroll.setName(result.getString("name"));
+					enroll.setName(result.getString("surname"));
+					enroll.setMail(result.getString("email"));
+					enroll.setMark(result.getString("mark"));
+					enroll.setStatus(Status.valueOf(result.getString("status")));
+					enrolls.add(enroll);
+				}
+			}
+		}
+		return enrolls;
+	}
+
 	public List<Enroll> FindEnrollsOrderedByStatusAsc(int exameDateId) throws SQLException {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY enroll.status ASC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam "
+				+ "WHERE IDExam = ? ORDER BY enroll.status ASC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -277,8 +313,8 @@ public class EnrollsDAO {
 		List<Enroll> enrolls = new ArrayList<>();
 
 		String query = "SELECT user.ID, user.name, user.surname, user.email, user.coursedeg, enroll.mark, enroll.status "
-				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.ID "
-				+ "WHERE examedate.ID = ? ORDER BY enroll.status DESC";
+				+ "FROM (user JOIN enroll ON user.ID = enroll.IDStudent) JOIN examdate ON enroll.IDExamDate = examdate.IDExam"
+				+ "WHERE IDExam = ? ORDER BY enroll.status DESC";
 		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, exameDateId);
 			try (ResultSet result = pstatement.executeQuery();) {
@@ -325,11 +361,11 @@ public class EnrollsDAO {
 	public void RefuseScore(int examDateId, int user_id) throws SQLException {
 
 		String query = "UPDATE projectdb.enroll SET status='REJECTED' WHERE IDExamDate = ? AND IDStudent = ?";
-		try (PreparedStatement pstatement = connection.prepareStatement(query);){
+		try (PreparedStatement pstatement = connection.prepareStatement(query);) {
 			pstatement.setInt(1, examDateId);
 			pstatement.setInt(2, user_id);
 			pstatement.executeUpdate();
-			}
+		}
 
 	}
 
