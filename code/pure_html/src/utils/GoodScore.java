24a9ca1243a01dc420219dec6b479fc0e982e32a
Francesco Paterna <francesco.paterna@mail.polimi.it>
2021-05-19 00:46:12 +0200

    update v0.40 CSS FIX

--- code/pure_html/src/utils/GoodScore.java
+++ code/pure_html/src/utils/GoodScore.java
@@ -7,7 +7,7 @@ public class GoodScore {
 	
 	private static String[] GoodScore = {"18","19","20","21","22","23","24","25","26","27","28", "29","30","30L"};
 	
-	private static String[] ValidScore = {"", "ABSENT", "RIMANDATO", "RIPROVATO", "18","19","20","21","22","23","24","25","26","27","28", "29","30","30L"};
+	private static String[] ValidScore = {"ABSENT", "RIMANDATO", "RIPROVATO", "18","19","20","21","22","23","24","25","26","27","28", "29","30","30L"};
 
 	
 	
