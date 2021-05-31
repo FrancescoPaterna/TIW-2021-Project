e5e94708766cfec2e738686a45aa7d5cd4468dac
Andrea Restelli <andrearestelli28@gmail.com>
2021-05-31 14:52:43 +0200

    added student courses visibility

--- code/RIA/WebContent/student.js
+++ code/RIA/WebContent/student.js
@@ -5,8 +5,7 @@
 (function() { // avoid variables ending up in the global scope
 
 	// page components
-	var missionDetails, missionsList, wizard,
-		pageOrchestrator = new PageOrchestrator(); // main controller
+	var courseList, pageOrchestrator = new PageOrchestrator(); // main controller
 
 	window.addEventListener("load", () => {
 		if (sessionStorage.getItem("id") == null) {
@@ -26,13 +25,86 @@
 			messagecontainer.textContent = this.username;
 		}
 	}
+	
+	function CourseList(_alert, _id_courseStud, _id_courseStudBody) {
+		this.alert = _alert;
+		this.courseStud = _id_courseStud;
+		this.courseStudBody = _id_courseStudBody;
+		
+		this.reset = function() {
+			this.courseStud.style.visibility = "hidden";
+		}
+		
+		this.show = function() {
+			var self = this; // used to refer to the current function from inner functions
+			makeCall("GET", "GetCourseStud", null,
+				function(req) {
+					if (req.readyState == 4) {
+						var message = req.responseText;
+						if (req.status == 200) {
+							var CoursesToShow = JSON.parse(req.responseText);
+							console.log('OI');
+							if (CoursesToShow.length == 0) {
+								self.alert.textContent = "no courses entered yet";
+								return;
+							}
+							self.update(CoursesToShow); // self visible by closure
+						}
+					} else {
+						self.alert.textContent = message;
+					}
+				}
+			);
+
+		};
+		
+		this.update = function(courseList){
+			var elem, i, row, destcell, linkcell, anchor;
+			this.courseStudBody.innerHTML = ""; // empty the table body
+			// build updated list
+			var self = this; // used to refer to the current function from inner functions
+			courseList.forEach(function(course) { // self visible here, not this
+				row = document.createElement("tr");
+				destcell = document.createElement("td");
+				destcell.textContent = course.id;
+				row.appendChild(destcell);
+				linkcell = document.createElement("td");
+				anchor = document.createElement("a");
+				linkcell.appendChild(anchor);
+				linkText = document.createTextNode(course.name);
+				anchor.appendChild(linkText);
+				//anchor.missionid = mission.id; // make list item clickable
+				anchor.setAttribute('course_id', course.id); // set a custom HTML attribute
+				anchor.addEventListener("click", (e) => {
+					// dependency via module parameter
+					courseDate.show(e.target.getAttribute("course_id")); // the list must know the details container
+				}, false); //TODO Repeat bubbling? 
+				anchor.href = "#";
+				row.appendChild(linkcell);
+				self.courseStudBody.appendChild(row);
+
+			});
+			this.courseStud.style.visibility = "visible";
+		}
+	}
 
 	function PageOrchestrator() {
+		var alertContainer = document.getElementById("id_alert");
 		this.start = function() {
 			var user = sessionStorage.getItem('name') + ' ' + sessionStorage.getItem('surname');
 			personalMessage = new PersonalMessage(user, document.getElementById("id_username"));
 			personalMessage.show();
 
+			courseList = new CourseList(
+				alertContainer,
+				document.getElementById("id_courseStud"),
+				document.getElementById("id_courseStudBody"));
 		}
+		
+		this.refresh = function(currentCourse) {
+			alertContainer.textContent = "";
+			courseList.reset();
+			courseList.show(); // closure preserves visibility of this
+		};
 	}
 })();
---
+++ code/RIA/src/controllers/GetCourseStud.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import beans.Course;
+import beans.User;
+import dao.CourseDAO;
+import utils.ConnectionHandler;
+
+/**
+ * Servlet implementation class GetCourseStud
+ */
+@WebServlet("/GetCourseStud")
+@MultipartConfig
+public class GetCourseStud extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection = null;
+       
+	public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+HttpSession session = request.getSession();
+		
+		User user = (User) session.getAttribute("user");
+		
+		CourseDAO courseDAO = new CourseDAO(connection);
+		List<Course> courses = new ArrayList<>();
+		
+		try {
+			courses = courseDAO.findCoursesByIdStudent(user.getId());
+		} catch (SQLException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to recover courses");
+			return;
+		}
+		
+		String serialized_courses = new Gson().toJson(courses);
+
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(serialized_courses);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
